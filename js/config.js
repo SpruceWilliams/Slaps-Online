bba97008915d36106f260987c08ae9e09524904e
SpruceWilliams <36278969+SpruceWilliams@users.noreply.github.com>
2025-12-24 12:55:30 +0100

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
 // js/config.js
+console.log("config.js loaded");
 window.API = {
   BASE: "https://script.google.com/macros/s/AKfycbxO9lWeFfz0mH4Spw5zQ67wsN3I1hpoHB89fkRJQ0ve9QsSL5j-FMrU09c7c-M2-s-o/exec"
 };
