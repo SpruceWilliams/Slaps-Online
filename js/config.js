0010c8ec71248f427bddf54074025abd2396407a
SpruceWilliams <36278969+SpruceWilliams@users.noreply.github.com>
2026-02-04 10:30:35 +0100

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // js/config.js
 console.log("config.js loaded");
 window.API = {
-  BASE: "https://script.google.com/macros/s/AKfycby9yCnLDj3Rp9oEL4sdhFk5M7-9P-Bgk0_eAXgVr8iVbiBKeH61YtzdtzZqnw30DVEE/exec"
+  BASE: "https://script.google.com/macros/s/AKfycbxxg-UAs0RLHKJZGnRqnOh4p0PFapY1biv_LWZic91v2QcCg7F-zLLj-7aZPRg3WpoF/exec"
 };
